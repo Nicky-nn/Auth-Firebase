7dc02ceac6c24229872047438f1f6396e89e196e
Nick Russell <nickynn07@gmail.com>
2020-09-18 00:53:45 +1200

    Finish

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const c = console.log;
 const d = document;
+const auth = firebase.auth();
+
+const linkOut = d.querySelectorAll('.logged-out')
+const linkIn = d.querySelectorAll('.logged-in')
+const loginCheck = user => {
+    if(user){
+        linkIn.forEach(link => link.style.display = 'block');
+        linkOut.forEach(link => link.style.display = 'none');
+    }
+    else{
+        linkOut.forEach(link => link.style.display = 'block');
+        linkIn.forEach(link => link.style.display = 'none');
+    }
+}
+
 // Regustarr Usuario
 const signupForm = d.querySelector("#signup-form");
 signupForm.addEventListener("submit", (e) => {
@@ -40,7 +55,6 @@ logout.addEventListener('click', e=> {
     e.preventDefault();
     auth.signOut().then(() => {
         $("#navbarNav").collapse('hide');        
-
         c('Saliendo');
     })
 })
@@ -64,9 +78,47 @@ const setupPost = data => {
         postList.innerHTML = html;
     }
     else{
-        postList.innerHTML = `<h5 class= "list-group-item list-group-item-action">Necesitas Entrar con tu Correo </h5>`
+        // postList.innerHTML = `<h5 class= "list-group-item list-group-item-action">Necesitas Entrar con tu Correo </h5>`
+        postList.innerHTML = `<h class= "list-group-item list-group-item-action">Necesitas Entrar con tu Correo </h>`
     }
 }
+//Google Login
+
+const google = d.querySelector('#googleLogin')
+google.addEventListener('click', e => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider)
+    .then(result => {
+        c('Logueado con Google')
+        signinForm.reset(); //Reseteamos input
+        $("#signinModal").modal("hide"); //Bootstrap el modal ocultamos
+        $("#navbarNav").collapse('hide');  
+    })
+    .catch(err => {
+        c(err)
+    })
+})
+
+//Facebook Login
+const facebook = d.querySelector('#faceLogin')
+facebook.addEventListener('click', e =>{
+    const provider = new firebase.auth.FacebookAuthProvider();
+    auth.signInWithPopup(provider)
+    .then(result => {
+        c('Logueado con Facebook')
+        signinForm.reset(); //Reseteamos input
+        $("#signinModal").modal("hide"); //Bootstrap el modal ocultamos
+        $("#navbarNav").collapse('hide');  
+    })
+    .catch(err => {
+        c(err)
+    })
+})
+
+
+
+
+
 //Eventos
 auth.onAuthStateChanged(user => { //Comprobar si esta logueado
     if(user){
@@ -74,12 +126,14 @@ auth.onAuthStateChanged(user => { //Comprobar si esta logueado
             .get()
             .then((snapshot) => {
                 setupPost(snapshot.docs)
+                loginCheck(user);
             })
     }
     else{
         c('Sin loguear')
-        postList.innerHTML = `<h5 class= "list-group-item list-group-item-action">Necesitas Entrar con tu Correo </h5>`
-
+        //postList.innerHTML = `<h5 class= "list-group-item list-group-item-action">Necesitas Entrar con tu Correo </h5>`
+        setupPost([]);
+        loginCheck(user);
     }
 })
 
